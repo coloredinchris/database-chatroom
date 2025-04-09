dfeb1ab01ab28a243ce1ffb7113a33bb958d9a67
Christopher Allen <vincentvalentine1320@yahoo.com>
2025-04-09 17:29:10 -0500

    adjusted some comments and uncommented LIVE testing

--- src/pages/ChatRoom.js
+++ src/pages/ChatRoom.js
@@ -7,11 +7,13 @@ import io from "socket.io-client";
 import "../styles/ChatRoom.css";
 import WelcomeScreen from "./WelcomeScreen";
 
+// Comment out the LIVE socket connection and uncomment the LOCAL one for local testing
 /**********           for testing LIVE             **********/
-//const socket = io("https://chatroom-backend-qv2y.onrender.com");
+const socket = io("https://chatroom-backend-qv2y.onrender.com");
 
+// Comment out the LOCAL socket connection and uncomment the LIVE one for production or LIVE testing
 /**********           for testing LOCAL            **********/
-const socket = io("http://localhost:5000");
+// const socket = io("http://localhost:5000");
 
 const readableColors = ["#3498db", "#9b59b6", "#1abc9c", "#f39c12", "#e67e22", "#e74c3c", "#2ecc71", "#34495e"];
 const getRandomReadableColor = () => readableColors[Math.floor(Math.random() * readableColors.length)];
